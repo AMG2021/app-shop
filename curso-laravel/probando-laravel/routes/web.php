87ea53ff61bacb3a6453794ae4d1b5eee2102d4c
AMG2021 <alvinmgomez01@gmail.com>
2018-07-26 14:51:23 -0400

    initial commit

---
+++ curso-laravel/probando-laravel/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+Route::get('/set_language/{lang}', 'Controller@setLanguage')->name('set_language');
+
+Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
+Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'courses'], function (){
+    Route::get('/{course}', 'CourseController@show')->name('courses.detail');
+
+});
+
+Route::get('/images/{path}/{attachment}', function($path, $attachment) {
+    $file = sprintf('storage/%s/%s', $path, $attachment);
+    if(File::exists($file)) {
+        return Image::make($file)->response();
+    }
+});
